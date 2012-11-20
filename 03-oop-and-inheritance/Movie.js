0127a99db3bcfdb8a019fe9986ac3fa1293e3ffb
Nicolas Batista <nicolasmbatista@gmail.com>
2012-11-20 14:35:28 -0300

    Changed Movie to Module
    Mixin now doesn't work.-

--- 03-oop-and-inheritance/Movie.js
+++ 03-oop-and-inheritance/Movie.js
@@ -1,3 +1,5 @@
+//This is my previous code. Before making a module out of it
+/*
 function Movie(){ 
 			this.attributes = {} ;
 			this.observers = [] ;
@@ -30,9 +32,9 @@ Movie.prototype.stop = function(){
 Movie.prototype.addObserver = function (observer){
 		this.observers.push(observer);
 	}
+*/
 
-
-
+/*
 function MovieObserver(){
 
 }
@@ -44,6 +46,7 @@ MovieObserver.prototype.notify = function(value,movie){
 		console.log(movie.getAttribute('name') + ' stopped.');
 	}
 }
+*/
 
 //Usage Example
 /*
@@ -55,3 +58,58 @@ terminator.addObserver(movieObserver);
 terminator.play();
 terminator.stop();
 */
+
+
+
+//Movie module:
+//****************************************************//
+
+//Now the MovieObserver class has a public function notify (it works the same)
+var MovieObserver = function(){
+	return {
+		notify : function(value,movie){
+			if(value == 'play'){
+				console.log('Playing ' + movie.getAttribute('name') + '...');
+			}else if (value == 'stop') {
+				console.log(movie.getAttribute('name') + ' stopped.');
+			}
+		}
+	}
+}
+
+//And Movie class has attributes and observers private to the user, along with a trigger function. Rest of the methods are public
+var Movie = function(){
+	var attributes = {};
+	var observers = [];
+	//I can't use "this" in this function, cause when its called, this = window (see play and stop)
+	var trigger = function(evt,movie){
+				for(var i = 0; i < observers.length ; i++){
+					observers[i].notify(evt,movie);
+				}
+				console.log('Event ' + evt + ' fired!');
+			};
+	
+	return {
+		getAttribute : function (key){
+			return attributes[key];
+			}
+
+		,setAttribute : function (key,value){
+				attributes[key] = value;
+			}
+			//So in play and stop, i have to pass a reference to me, so trigger can "send me" along with the event
+		,play : function(){
+				trigger('play',this);
+			}
+
+		,stop : function(){
+				trigger('stop',this);
+			}
+
+		,addObserver : function (observer){
+			observers.push(observer);
+		}
+		
+	}
+}
+
