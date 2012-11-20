45c520a9517541d5773b50950a3b1c358b8859be
Nicolas Batista <nicolasmbatista@gmail.com>
2012-11-20 00:59:02 -0300

    Added Points 8-10
    Now it gets the info from the web service!

--- 03-oop-and-inheritance/Movie.js
+++ 03-oop-and-inheritance/Movie.js
@@ -4,54 +4,54 @@ function Movie(){
 
 		}
 
-		Movie.prototype.getAttribute = function (key){
-				return this.attributes[key];
-			}
+Movie.prototype.getAttribute = function (key){
+		return this.attributes[key];
+	}
 
-		Movie.prototype.setAttribute = function (key,value){
-				this.attributes[key] = value;
-			}
+Movie.prototype.setAttribute = function (key,value){
+		this.attributes[key] = value;
+	}
 
-		Movie.prototype.trigger = function(evt){
-				for(var i = 0; i < this.observers.length ; i++){
-					this.observers[i].notify(evt,this);
-				}
-				console.log('Event' + evt + 'fired!');
-			}
-		Movie.prototype.play = function(){
-				this.trigger('play');
-
-			}
-
-		Movie.prototype.stop = function(){
-				this.trigger('stop');
-			}
-
-		Movie.prototype.addObserver = function (observer){
-				this.observers.push(observer);
-			}
-		
-
-
-		function MovieObserver(){
-
+Movie.prototype.trigger = function(evt){
+		for(var i = 0; i < this.observers.length ; i++){
+			this.observers[i].notify(evt,this);
 		}
+		console.log('Event ' + evt + ' fired!');
+	}
+Movie.prototype.play = function(){
+		this.trigger('play');
 
-		MovieObserver.prototype.notify = function(value,movie){
-			if(value == 'play'){
-				console.log('Playing ' + movie.getAttribute('name') + '...');
-			}else if (value == 'stop') {
-				console.log(movie.getAttribute('name') + ' stopped.');
-			}
-		}
-		
-		//Usage Example
-		/*
-		var terminator = new Movie();
-		terminator.setAttribute('name','Terminator');
-		terminator.setAttribute('year','1990');
-		var movieObserver = new MovieObserver();
-		terminator.addObserver(movieObserver);
-		terminator.play();
-		terminator.stop();
-		*/
+	}
+
+Movie.prototype.stop = function(){
+		this.trigger('stop');
+	}
+
+Movie.prototype.addObserver = function (observer){
+		this.observers.push(observer);
+	}
+
+
+
+function MovieObserver(){
+
+}
+
+MovieObserver.prototype.notify = function(value,movie){
+	if(value == 'play'){
+		console.log('Playing ' + movie.getAttribute('name') + '...');
+	}else if (value == 'stop') {
+		console.log(movie.getAttribute('name') + ' stopped.');
+	}
+}
+
+//Usage Example
+/*
+var terminator = new Movie();
+terminator.setAttribute('name','Terminator');
+terminator.setAttribute('year','1990');
+var movieObserver = new MovieObserver();
+terminator.addObserver(movieObserver);
+terminator.play();
+terminator.stop();
+*/
