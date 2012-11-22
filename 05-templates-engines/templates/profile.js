bd4ecd8d8d97b23144150a3c27dfacfb94dc21ca
Jojo <nicolasmbatista@gmail.com>
2012-11-22 20:23:12 -0300

    Added Dust template

---
+++ 05-templates-engines/templates/profile.js
@@ -0,0 +1,13 @@
+(function() {
+  dust.register("dust", body_0);
+
+  function body_0(chk, ctx) {
+    return chk.write("<div class=\"profile-dust\" style=\"text-align:center\"><h1><u>Dust</u></h1><table align=center border=1><tr><td><img src = \"").reference(ctx.get("profileImg"), ctx, "h").write("\"></img></td><td><h1>").reference(ctx.get("name"), ctx, "h").write("</h1><h2>").reference(ctx.get("currentJob"), ctx, "h").write("</h2><h3>").reference(ctx.get("location"), ctx, "h").write(" | ").reference(ctx.get("industry"), ctx, "h").write("</h3></td></tr><tr><td colspan=2><h3>Personal description</h3><p>\t\t  ").reference(ctx.get("description"), ctx, "h").write("</p><h3>Job Experience</h3><ul>").section(ctx.get("jobs"), ctx, {
+      "block": body_1
+    }, null).write("</ul></td></tr></table></div>");
+  }
+  function body_1(chk, ctx) {
+    return chk.write("<li>").reference(ctx.get("company"), ctx, "h").write(", ").reference(ctx.get("description"), ctx, "h").write("</li>");
+  }
+  return body_0;
+})();
