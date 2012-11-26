c16c4303cadd0f7f78b3759348c6c81834633d45
Jojo <nicolasmbatista@gmail.com>
2012-11-26 20:05:08 -0300

    requirejs strikes again

--- 06-backbone/Scripts/main.js
+++ 06-backbone/Scripts/main.js
@@ -1,4 +1,32 @@
-require(["lib/jquery","lib/jquerymobile","lib/handlebars"], function() {
+requirejs.config({
+    shim: {
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'foo': {
+            deps: ['bar'],
+            exports: 'Foo',
+            init: function (bar) {
+                //Using a function allows you to call noConflict for
+                //libraries that support it, and do other cleanup.
+                //However, plugins for those libraries may still want
+                //a global. "this" for the function will be the global
+                //object. The dependencies will be passed in as
+                //function arguments. If this function returns a value,
+                //then that value is used as the module export value
+                //instead of the object found via the 'exports' string.
+                return this.Foo.noConflict();
+            }
+        }
+    }
+});
+
+require(["lib/backbone","lib/handlebars","app/model/movieModel"], function(underscore,movieModel) {
 	var myProfile = {
 				name : "Nicolas Batista"
 				,currentJob : "Student at UNCPBA"
@@ -25,6 +53,9 @@ require(["lib/jquery","lib/jquerymobile","lib/handlebars"], function() {
 	var template = Handlebars.compile(source);
 	var context = myProfile
 	var html    = template(context);
-	$('#body').html(html);
+	$('body').html(html);
 	//$.mobile.hidePageLoadingMsg();
+
+
+
 });
